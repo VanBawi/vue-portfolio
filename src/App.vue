647a5913c94185f354f441516b5461b96893d708
Jack Van <superjackpower85@gmail.com>
2020-06-26 01:57:11 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<body-content></body-content>
+	</div>
+</template>
+
+<script>
+import HeaderNav from './components/HeaderNav';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import BodyContent from './components/BodyContent';
+import Store from './components/Store';
+
+export default {
+	components: {
+		'header-nav': HeaderNav,
+		'sign-in': SignIn,
+		'sign-up': SignUp,
+		'body-content': BodyContent,
+		'store-list': Store,
+	},
+	name: 'app',
+	data() {
+		return {
+			msg: 'Welcome to Your Vue.js App',
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/BodyContent.vue
@@ -0,0 +1,564 @@
+<template>
+	<div>
+		<!-- /.responsive_menu -->
+		<header class="site-header clearfix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 pull-all">
+						<div class="main-navigation pull-right">
+							<nav class="main-nav visible-md visible-lg">
+								<ul class="sf-menu">
+									<li class="active"><a href="index.html">Home</a></li>
+									<li><a href="#">Portfolio</a></li>
+									<li><a href="#">Blog</a></li>
+									<li><a href="archives.html">Archives</a></li>
+									<li><a href="contact.html">Contact</a></li>
+									<h3>Jecko</h3>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<!-- /.site-header -->
+		<section id="homeIntro" class="parallax first-widget">
+			<div class="parallax-overlay">
+				<div class="container home-intro-content">
+					<div class="row">
+						<div class="col-md-12">
+							<h2>How coding has changed the world for the better</h2>
+							<div class="parallax-text col-md-6">
+								<p class="text-post">
+									Who says you can’t change the world? History has repeatedly shown that whatever
+									isn’t possible today can be possible tomorrow.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- /#homeIntro -->
+		<section class="light-content services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-4">
+						<div class="service-box-wrap">
+							<div class="service-icon-wrap">
+								<i class="fa fa-umbrella fa-2x"></i>
+							</div>
+							<div class="service-cnt-wrap">
+								<h3 class="service-title">Easy Theme Colors</h3>
+								<p>There are 4 color themes (blue, green, red, orange) and this one is blue.</p>
+							</div>
+						</div>
+					</div>
+					<!-- /.col-md-4 -->
+					<div class="col-md-7 col-sm-7">
+						<div class="service-box-wrap">
+							<div class="service-icon-wrap">
+								<i class="fa fa-pencil-square-o fa-2x"></i>
+							</div>
+							<!-- /.service-icon-wrap -->
+							<div class="service-cnt-wrap">
+								<h3 class="service-title">Pixel Perfect Design</h3>
+								<p>Based on a 12 column grid system, with every pixel snapped.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4 col-sm-4">
+						<div class="service-box-wrap">
+							<div class="service-icon-wrap">
+								<i class="fa fa-code fa-2x"></i>
+							</div>
+							<!-- /.service-icon-wrap -->
+							<div class="service-cnt-wrap">
+								<h3 class="service-title">Valid HTML5</h3>
+								<p>We offer validated html files and well commented code on our themes.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-7 col-sm-7">
+						<div class="service-box-wrap">
+							<div class="service-icon-wrap">
+								<i class="fa fa-suitcase fa-2x"></i>
+							</div>
+							<!-- /.service-icon-wrap -->
+							<div class="service-cnt-wrap">
+								<h3 class="service-title">Special Thanks</h3>
+								<p>
+									Credit goes to
+									<a rel="nofollow" href="http://unsplash.com" target="_blank">Unsplash</a> for
+									images used in this template.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="testimonials-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="bxslider">
+							<div class="testimonial">
+								<div class="testimonial-content">
+									<span class="testimonial-author">Andy Colin - Architect</span>
+									<p class="testimonial-description">
+										Thank you for all your good work in creating theme. They have a ‘presence’ which
+										feels right.
+									</p>
+								</div>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-content">
+									<span class="testimonial-author">Thomas Teddy - Design Writer</span>
+									<p class="testimonial-description">
+										I love the logo. Particularly how the mark can stand on its own. Nice work! It
+										feels tall and proud and powerful — and I love that. That’s what I was after.
+									</p>
+								</div>
+							</div>
+							<div class="testimonial">
+								<div class="testimonial-content">
+									<span class="testimonial-author">John Willy - Developer</span>
+									<p class="testimonial-description">
+										You’re pretty awesome to work with. Incredibly organized, easy to communicate
+										with, responsive with next iterations, and beautiful work.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="light-content our-team">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-header">
+							<h2 class="section-title">Meet our happy staff</h2>
+							<p class="section-desc">
+								Medigo is the automated way to keep track of what everyone is working on.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.container -->
+			<div class="team-members">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-sm-4">
+							<div class="team-member">
+								<div class="thumb-post">
+									<img src="images/includes/member1.jpg" alt="" />
+									<span class="member-role">Marketing Manager</span>
+								</div>
+								<div class="member-content">
+									<h4 class="member-name">Amy Groovy</h4>
+									<p>Duis vitae consequat neque. Nulla pharetra eleifend nulla.</p>
+								</div>
+							</div>
+							<!-- /.team-member -->
+						</div>
+						<!-- /.col-md-4 -->
+
+						<div class="col-md-4 col-sm-4">
+							<div class="team-member">
+								<div class="thumb-post">
+									<img src="images/includes/member2.jpg" alt="" />
+									<span class="member-role">Web Developer</span>
+								</div>
+								<div class="member-content">
+									<h4 class="member-name">Candy Sharp</h4>
+									<p>Duis vitae consequat neque. Nulla pharetra eleifend nulla.</p>
+								</div>
+							</div>
+							<!-- /.team-member -->
+						</div>
+						<!-- /.col-md-4 -->
+
+						<div class="col-md-4 col-sm-4">
+							<div class="team-member">
+								<div class="thumb-post">
+									<img src="images/includes/member3.jpg" alt="" />
+									<span class="member-role">Graphic Designer</span>
+								</div>
+								<div class="member-content">
+									<h4 class="member-name">Linda Master</h4>
+									<p>Duis vitae consequat neque. Nulla pharetra eleifend nulla.</p>
+								</div>
+							</div>
+							<!-- /.team-member -->
+						</div>
+						<!-- /.col-md-4 -->
+					</div>
+					<!-- /.row -->
+				</div>
+				<!-- /.container -->
+			</div>
+			<!-- /.team-members -->
+		</section>
+		<!-- /.our-team -->
+
+		<section id="blogPosts" class="parallax">
+			<div class="parallax-overlay">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="section-header">
+								<h2 class="section-title">Recent from our blog</h2>
+								<p class="section-desc">Everything you need to create a professional website.</p>
+							</div>
+							<!-- /.section-header -->
+						</div>
+						<!-- /.col-md-12 -->
+					</div>
+					<!-- /.row -->
+					<div class="row latest-posts">
+						<div class="col-md-4 col-sm-6">
+							<div class="blog-post clearfix">
+								<div class="thumb-post">
+									<a href="blog-single.html"
+										><img src="images/includes/blogthumb1.jpg" alt="" class="img-circle"
+									/></a>
+								</div>
+								<div class="blog-post-content">
+									<h4 class="post-title">
+										<a href="blog-single.html">Getting Creative With the Google Maps API</a>
+									</h4>
+									<span class="meta-post-date">12 February 2084</span>
+								</div>
+							</div>
+							<!-- /.blog-post -->
+						</div>
+						<!-- /.col-md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="blog-post clearfix">
+								<div class="thumb-post">
+									<a href="blog-single.html"
+										><img src="images/includes/blogthumb2.jpg" alt="" class="img-circle"
+									/></a>
+								</div>
+								<div class="blog-post-content">
+									<h4 class="post-title">
+										<a href="blog-single.html">Design Deliverables – easily share project</a>
+									</h4>
+									<span class="meta-post-date">10 February 2084</span>
+								</div>
+							</div>
+							<!-- /.blog-post -->
+						</div>
+						<!-- /.col-md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="blog-post clearfix">
+								<div class="thumb-post">
+									<a href="blog-single.html"
+										><img src="images/includes/blogthumb3.jpg" alt="" class="img-circle"
+									/></a>
+								</div>
+								<div class="blog-post-content">
+									<h4 class="post-title">
+										<a href="blog-single.html">Using Templates to Get Your Clients Thinking</a>
+									</h4>
+									<span class="meta-post-date">8 February 2084</span>
+								</div>
+							</div>
+							<!-- /.blog-post -->
+						</div>
+						<!-- /.col-md-4 -->
+					</div>
+					<!-- /.row -->
+				</div>
+				<!-- /.container -->
+			</div>
+			<!-- /.parallax-overlay -->
+		</section>
+		<!-- /#blogPosts -->
+
+		<footer class="site-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<nav class="footer-nav clearfix">
+							<ul class="footer-menu">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="portfolio.html">Portfolio</a></li>
+								<li><a href="blog.html">Blog Posts</a></li>
+								<li><a href="archives.html">Shortcodes</a></li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+							<!-- /.footer-menu -->
+						</nav>
+						<!-- /.footer-nav -->
+					</div>
+					<!-- /.col-md-12 -->
+				</div>
+				<!-- /.row -->
+				<div class="row">
+					<div class="col-md-12">
+						<p class="copyright-text">Copyright &copy; 2084 Company Name | Design: templatemo</p>
+					</div>
+					<!-- /.col-md-12 -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.container -->
+		</footer>
+		<!-- /.site-footer -->
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+body {
+	background: white;
+	font-size: 16px;
+	font-family: 'PT Serif', Georgia, serif;
+	font-weight: normal;
+	font-style: normal;
+	line-height: 28px;
+	color: #777777;
+	-webkit-font-smoothing: antialiased;
+	-webkit-text-size-adjust: 100%;
+}
+
+.footer-menu,
+.copyright-text {
+	text-align: center;
+}
+
+.main-nav ul,
+.main_menu,
+.main_menu li ul,
+.footer-menu {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+.first-widget {
+	position: relative;
+	margin-top: 61px;
+	height: 75vh;
+}
+
+.light-content,
+#blogPosts {
+	margin-top: 80px;
+}
+
+/*------------------------------------------------------
+	HEADER STYLES
+--------------------------------------------------------*/
+.site-header {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	background: #fff;
+	z-index: 9999;
+	padding: 10px 0;
+	margin-bottom: 30px;
+	overflow: visible;
+	transition: padding 0.3s ease-in-out;
+	-moz-transition: padding 0.3s ease-in-out;
+	-webkit-transition: padding 0.3s ease-in-out;
+}
+.site-header.site-header-shrink {
+	padding: 17px 0;
+}
+
+.main-nav ul li {
+	float: right;
+	display: block;
+	margin-left: 20px;
+	position: relative;
+}
+.main-nav ul li a {
+	color: #31373a;
+	font-size: 16px;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-family: 'Raleway', sans-serif;
+	padding: 5px 15px;
+	display: block;
+	-webkit-border-radius: 3px;
+	-moz-border-radius: 3px;
+	-ms-border-radius: 3px;
+	-o-border-radius: 3px;
+	border-radius: 3px;
+}
+.main-nav ul li a:hover {
+	background-color: #f1f4f5;
+	text-decoration: none;
+	color: #3978d6;
+}
+.main-nav ul li a i {
+	margin-left: 7px;
+}
+.main-nav ul li.active a {
+	text-decoration: none;
+	color: #0e4ba7;
+	background-color: #f1f4f5;
+}
+
+.main-nav ul li ul li a {
+	background-color: transparent !important;
+	color: #777777 !important;
+	text-transform: capitalize;
+	font-weight: 600;
+}
+
+#homeIntro {
+	background-image: url('../assets/images/wallpaper1.jpg');
+}
+
+.parallax {
+	background-attachment: fixed;
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: cover;
+	color: #fff;
+	overflow: hidden;
+}
+
+.parallax-overlay {
+	background-color: rgba(0, 0, 0, 0.3);
+}
+.parallax-text {
+	width: 40%;
+	margin: 0 auto;
+}
+
+.text-post:hover {
+	padding: 20px;
+}
+.text-post {
+	background-color: lightgrey;
+	margin: 0 auto;
+	padding: 15px;
+	opacity: 0.4;
+	font-weight: 600;
+	box-shadow: 0 0 1px gray;
+	-webkit-border-radius: 3px;
+	-moz-border-radius: 3px;
+	-ms-border-radius: 3px;
+	-o-border-radius: 3px;
+	border-radius: 3px;
+	transition: padding 0.5s ease-in-out;
+	-moz-transition: padding 0.5s ease-in-out;
+	-webkit-transition: padding 0.5s ease-in-out;
+}
+.text-post {
+	color: #000;
+}
+
+.pageTitle {
+	padding-top: 70px;
+	padding-bottom: 70px;
+}
+
+.page-title {
+	color: white;
+	margin: 5px 0 0 0;
+	font-size: 30px;
+	font-weight: 800;
+}
+.home-intro-content {
+	padding: 11em 0;
+	text-align: center;
+}
+.home-intro-content h2 {
+	font-size: 36px;
+	font-weight: 800;
+	color: white;
+}
+.home-intro-content p {
+	margin: 55px 0;
+	font-size: 18px;
+	line-height: 32px;
+}
+.home-intro-content .large-button:hover {
+	opacity: 0.8;
+}
+
+blockquote {
+	margin: 30px 0;
+	display: block;
+	color: #aaaaaa;
+}
+
+.site-footer {
+	background-color: #f1f4f5;
+	padding: 100px 0;
+	color: #aaaaaa;
+	font-family: 'Raleway', sans-serif;
+}
+
+.footer-menu li {
+	display: inline-block;
+	margin: 0 20px;
+}
+.footer-menu li a {
+	color: #aaaaaa;
+	font-size: 13px;
+	text-transform: uppercase;
+	font-weight: 700;
+}
+.footer-menu li a:hover {
+	color: #00a8d6;
+}
+
+.copyright-text {
+	margin-top: 30px;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 13px;
+}
+
+// RESPONSIVE STYLES
+
+@media (max-width: 991px) {
+	.site-header {
+		padding: 17px 0;
+		position: relative !important;
+	}
+	.first-widget {
+		margin-top: -30px;
+		height: 60vh;
+	}
+}
+@media (max-width: 767px) {
+	.parallax {
+		background-position: center center !important;
+	}
+	.parallax-text {
+		width: 100%;
+	}
+	.cta .cta-title {
+		display: block;
+		margin-bottom: 30px;
+	}
+
+	.pageTitle {
+		text-align: center;
+	}
+	.pageTitle .text-right {
+		text-align: center;
+	}
+}
+</style>
